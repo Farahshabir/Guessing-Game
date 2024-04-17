2db8ec57585f514076f906d5024e206e396b8bba
Farahshabir <farah.shabir@hotmail.com>
2024-04-17 20:11:43 +0500

    Created number guessing game

---
+++ index.ts
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+//console.log(randomNumber);
+
+const answers = await inquirer.prompt([
+  {
+    name: "userGuessedNumber",
+    type: "number",
+    message: "Guess a number between 1 to 10:",
+  },
+]);
+
+if (answers.userGuessedNumber === randomNumber) {
+  console.log("Congratulations! you guessed right number");
+} else {
+  console.log("You guessed wrong number");
+}
